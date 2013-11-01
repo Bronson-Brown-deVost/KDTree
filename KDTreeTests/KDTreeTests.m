083bb30f7f4313301f0739698d12823401b601cf
shaknum <bronsonbdeovst@aim.com>
2013-11-01 18:50:15 -0400

    first commit

---
+++ KDTreeTests/KDTreeTests.m
@@ -0,0 +1,34 @@
+//
+//  KDTreeTests.m
+//  KDTreeTests
+//
+//  Created by Bronson Brown-deVost on 11/1/13.
+//  Copyright (c) 2013 Bronson Brown-deVost. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KDTreeTests : XCTestCase
+
+@end
+
+@implementation KDTreeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
